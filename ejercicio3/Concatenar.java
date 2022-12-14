91f967e628f34b2a8b6d40a41024cf521098ef6a
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-14 12:25:50 +0100

    comentario

--- ejercicio3/Concatenar.java
+++ ejercicio3/Concatenar.java
@@ -1,5 +1,9 @@
 package ejercicio3;
 
+/* 
+En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
+Tened en cuenta que los textos pueden venir de un array de tipo String.  
+*/
 public class Concatenar {
 
     public static void main(String[] args) {
