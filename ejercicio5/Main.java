eb7ba135ce48aba6080c2f96e294006db59a3bc8
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-15 08:58:55 +0100

    comment

--- ejercicio5/Main.java
+++ ejercicio5/Main.java
@@ -1,5 +1,12 @@
 package ejercicio5;
 
+/* 
+Crear una interfaz CocheCRUD.
+Crear una implementación CocheCRUDImpl que implemente la interfaz CocheCRUD.
+Como métodos de CocheCRUD podemos poner:
+save() findAll() delete() que simplemente impriman por consola el nombre del propio método.
+Desde una clase Main, Crear un objeto de tipo CocheCRUDImpl y llamar a cada uno de los métodos.
+*/
 public class Main {
     
     static CocheCRUD cocheCRUD = new CocheCRUDimpl() {
